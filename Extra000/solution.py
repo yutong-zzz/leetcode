69a83d7f6c23c021399d3f0e113d336ac3465308
wyt <dayujun930@gmail.com>
2018-04-01 00:02:24 +0800

    add solution
    recurrent version

---
+++ Extra000/solution.py
@@ -0,0 +1,29 @@
+class ListNode():
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+def reverselist(ll):
+    cur = ll
+    pre = None
+    l = ll
+    while cur:
+        l = cur
+        tmp = cur.next
+        cur.next = pre
+        pre = cur
+        cur = tmp
+    return l
+
+
+head=ListNode(1);
+p1=ListNode(2);
+p2=ListNode(3); 
+p3=ListNode(4); 
+head.next=p1; 
+p1.next=p2; 
+p2.next=p3; 
+p=reverselist(head); 
+while p: 
+  print p.val; 
+  p=p.next;
